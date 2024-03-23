7560c7d53fc43e0bc9a447285da2b6b5a431286d
Rylan Baun <rylanjbaun@gmail.com>
2024-03-22 20:40:06 -0600

    Change state management in App.jsx component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import useApplicationData from 'hooks/useApplicationData';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photos from 'mocks/photos';
@@ -8,33 +9,16 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-
-  const [favourites, setFavourites] = useState([])
-  const [iconFill, setIconFill] = useState(false)
-
-  const saveFavourites = (photo) => {
-    if (!favourites.some(fav => fav.id === photo.id)) {
-      setFavourites(prevPhotos => [...prevPhotos, photo])
-      setIconFill(true)
-    } else {
-      setFavourites(prevPhotos => prevPhotos.filter(fav => fav.id !== photo.id))
-      favourites.length ? setIconFill(true) : setIconFill(false)
-    }
-    
-  }
-
-  const [ modal, setModal ] = useState(false)
-  const [ modalObj, setModalObj ] = useState(photos[0]);
-
-  const toggleModal = (photoObj=photos[0]) => {
-    setModal(prev => !prev)
-    setModalObj(prev => photoObj)
-  }
+  const {
+    state,
+    saveFavourites,
+    toggleModal
+  } = useApplicationData();
 
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos} favouriteExists={!!favourites.length} toggleModal={toggleModal} favourites={favourites} saveFavourites={saveFavourites} iconFill={iconFill}/>
-      <PhotoDetailsModal visible={modal} toggleModal={toggleModal} photo={modalObj} favourites={favourites} saveFavourites={saveFavourites} iconFill={iconFill}/>
+      <HomeRoute topics={topics} photos={photos} favouriteExists={!!state.favourites.length} toggleModal={toggleModal} favourites={state.favourites} saveFavourites={saveFavourites} iconFill={state.iconFill}/>
+      <PhotoDetailsModal visible={state.modal} toggleModal={toggleModal} photo={state.modalObj} favourites={state.favourites} saveFavourites={saveFavourites} iconFill={state.iconFill}/>
     </div>
   );
 };
